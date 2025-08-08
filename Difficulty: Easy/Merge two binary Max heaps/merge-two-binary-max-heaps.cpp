5fe7cd1ba0e33eb6196ba52aeb40870dd1959694
Somnath0407 <somnathmondal2121@gmail.com>
2025-08-08 23:24:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Merge two binary Max heaps/merge-two-binary-max-heaps.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        priority_queue<int> pq;
+
+        for (int i = 0; i < n; i++) pq.push(a[i]);
+        for (int i = 0; i < m; i++) pq.push(b[i]);
+
+        vector<int> res;
+        while (!pq.empty()) {
+            res.push_back(pq.top());
+            pq.pop();
+        }
+        return res;
+    }
+};
