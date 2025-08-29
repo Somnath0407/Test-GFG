54b946f2c9d114b31f9e020c8082ff5a5e9b119c
Somnath0407 <somnathmondal2121@gmail.com>
2025-08-29 15:18:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth smallest element in a Matrix/kth-smallest-element-in-a-matrix.cpp
+++ Difficulty: Medium/Kth smallest element in a Matrix/kth-smallest-element-in-a-matrix.cpp
@@ -1,26 +1,19 @@
 class Solution {
   public:
-    int kthSmallest(vector<vector<int>> &mat, int k) {
+    int kthSmallest(vector<vector<int>>& mat, int k) {
         int n = mat.size();
-        vector<pair<int,pair<int,int>>> temp;
-        for(int i=0; i<n; i++) {
-            temp.push_back(make_pair(mat[i][0],make_pair(i,0)));
-        }
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> p(temp.begin(),temp.end()); 
-        int ans;
-        pair<int,pair<int,int>> element;
-        int i,j;
-
-        while(k--){
-            element = p.top();
-            p.pop();
-            ans = element.first;
-            i = element.second.first;
-            j = element.second.second;
-            if(j+1 < mat[i].size()){
-                p.push(make_pair(mat[i][j+1],make_pair(i,j+1)));
+        int start = mat[0][0], end = mat[n-1][n-1];
+        
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                count += upper_bound(mat[i].begin(), mat[i].end(), mid) - mat[i].begin();
             }
+            if (count < k) start = mid + 1;
+            else end = mid;
         }
-        return ans;
+        
+        return start;
     }
 };
