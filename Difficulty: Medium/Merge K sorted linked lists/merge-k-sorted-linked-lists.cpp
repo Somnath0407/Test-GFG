5c4d57766f7e35add794f93ec75770df3c59a564
Somnath0407 <somnathmondal2121@gmail.com>
2025-09-07 19:37:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.cpp
+++ Difficulty: Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.cpp
@@ -1,38 +1,49 @@
-// class Node {
-//   public:
-//     int data;
-//     Node* next;
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
 
-//     Node(int x){
-//         data = x;
-//         next = NULL;
-//     }
-// };
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
 
 class Solution {
   public:
-    Node* mergeKLists(vector<Node*>& arr) {
-        struct compare {
-            bool operator()(Node* a, Node* b) {
-                return a->data > b->data; // min-heap
-            }
-        };
-
-        priority_queue<Node*, vector<Node*>, compare> pq;
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i]) pq.push(arr[i]);
-        }
-
+    Node* mergeTwoLists(Node* l1, Node* l2) {
         Node* dummy = new Node(0);
         Node* tail = dummy;
-
-        while (!pq.empty()) {
-            Node* t = pq.top();
-            pq.pop();
-            tail->next = t;
+        while (l1 && l2) {
+            if (l1->data <= l2->data) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
             tail = tail->next;
-            if (t->next) pq.push(t->next);
         }
-        return dummy->next;
+        if (l1){
+            tail->next = l1;
+        }
+        if (l2) {
+        tail->next = l2;
+        }
+        
+        Node* res = dummy->next;
+        delete dummy;
+        return res;
+    }
+    Node* mergeKLists(vector<Node*>& arr) {
+        if (arr.empty()) return NULL;
+
+        Node* head = arr[0];
+        for (int i = 1; i < arr.size(); i++) {
+            head = mergeTwoLists(head, arr[i]);
+        }
+        return head;
     }
 };
