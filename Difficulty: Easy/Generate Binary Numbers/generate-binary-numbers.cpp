6f22ea05c755f1192518a78f41743fb9ae30d34b
Somnath0407 <somnathmondal2121@gmail.com>
2025-09-25 11:37:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Generate Binary Numbers/generate-binary-numbers.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    vector<string> generateBinary(int n) {
+       vector<string> ans;
+        for(int i = 1; i <= n; i++) {
+            string s = bitset<32>(i).to_string();  
+            ans.push_back(s.substr(s.find('1')));  
+        }
+        return ans; 
+    }
+};
