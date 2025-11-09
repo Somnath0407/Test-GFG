a15e60cce7e062481426fb901c5c2c6ffafb7dc2
Somnath0407 <somnathmondal2121@gmail.com>
2025-11-09 19:53:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Words/reverse-words.cpp
+++ Difficulty: Easy/Reverse Words/reverse-words.cpp
@@ -1,26 +1,20 @@
 class Solution {
   public:
     string reverseWords(string &s) {
-        vector<string> words;
-        string temp = "";
-        for (char c : s) {
-            if (c == '.') {
-                if (!temp.empty()) {
-                    words.push_back(temp);
-                    temp.clear();
-                }
-            } else temp += c;
-        }
-        if (!temp.empty())
-        words.push_back(temp);
-        
-        reverse(words.begin(), words.end());
-        
+        reverse(s.begin(), s.end());
+        int n = s.size();
+        int start = 0;
         string res = "";
-        for (int i = 0; i < words.size(); i++) {
-            res += words[i];
-            if (i < words.size() - 1) 
-            res += '.';
+        for (int i = 0; i <= n; ++i) {
+            if (i == n || s[i] == '.') {
+                int len = i - start;
+                if (len > 0) {                 
+                    reverse(s.begin() + start, s.begin() + i); 
+                    if (!res.empty()) res.push_back('.');
+                    res.append(s.substr(start, len));
+                }
+                start = i + 1;
+            }
         }
         return res;
     }
