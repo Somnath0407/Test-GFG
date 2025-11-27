1575b4f4e2ed407192afb373681c791a2a30eac3
Somnath0407 <somnathmondal2121@gmail.com>
2025-11-27 21:56:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/All Subsets Xor Sum/all-subsets-xor-sum.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int subsetXORSum(vector<int>& arr) {
+        int n = arr.size(), OR = 0;
+        for(int x : arr) OR |= x;
+        return OR * (1 << (n - 1));
+    }
+};
