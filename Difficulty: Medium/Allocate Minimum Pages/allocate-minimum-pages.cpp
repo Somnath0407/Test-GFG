8a2d4b993f639b43879d271a3448bb92ab051ac4
Somnath0407 <somnathmondal2121@gmail.com>
2026-03-15 13:25:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,36 +1,48 @@
 class Solution {
   public:
+   int maxarray(vector<int> &arr,int n){
+      int res=arr[0];
+      for(int i=1;i<n;i++){
+          res=max(res,arr[i]);
+      }
+      return res;
+  }
+  int totalarray(vector<int> &arr,int n){
+      int sum=0;
+      for(int i=0;i<n;i++){
+          sum+=arr[i];
+      }
+      return sum;
+  }
+  
+  bool fun(vector<int> &arr,int n,int mid,int k){
+      int student=1;
+      int page=0;
+      for(int i=0;i<n;i++){
+          if(page+arr[i]<=mid){
+              page+=arr[i];
+          }
+          else{
+              student++;
+              page=arr[i];
+              if(student>k) return ;
+          }
+      }
+      return 1;
+  }
     int findPages(vector<int> &arr, int k) {
         int n=arr.size();
-        if(k>n) return -1;
-        
-        int low = arr[0], high = 0;
-        for (int i = 0; i < n; i++) {
-            low = max(low, arr[i]);  
-            high += arr[i];          
-        }
-
-        int ans = -1;
+        if(n<k) return -1;
+        int low=maxarray(arr,n),high=totalarray(arr,n);
+        int res=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            int count=1,page=0;
-            for(int i=0;i<n;i++){
-                if(page+arr[i]>mid){
-                    count++;
-                    page=arr[i];
-                }
-                else{
-                    page+=arr[i];
-                }
-            }
-            if(count<=k){
-                ans=mid;
+            if(fun(arr,n,mid,k)){
+                res=mid;
                 high=mid-1;
             }
-            else{
-                low=mid+1;
-            }
+            else low=mid+1;
         }
-        return ans;
+        return res;
     }
 };
